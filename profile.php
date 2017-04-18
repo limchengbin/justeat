13d7f15a1301a12cd48f7c36cbcc11ce82e18e0b
Jin <jinlim0101@hotmail.com>
2017-04-18 22:19:25 +0100

    profile done abit nia

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@ and open the template in the editor.
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Home</title>
+        <title>Profile</title>
 
         <!-- Bootstrap Core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -20,38 +20,37 @@ and open the template in the editor.
         <!-- Custom CSS -->
         <link href="css/stylish-portfolio.css" rel="stylesheet">
         <link href="css/navbar.css" rel="stylesheet" type="text/css"/>
-        <link href="css/home.css" rel="stylesheet" type="text/css"/>
 
         <!-- Custom Fonts -->
         <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        
+        <style>
+            .profile-pic{
+                max-width: 100%;
+                height: auto;
+                background-color: yellow;
+            }
+            
+            .username{
+                background-color: blue;
+            }
+        </style>
     </head>
     <body>
-        <?php include 'include/navbar.php' ?>
-        <div class="container big-container">
-            <div class="col-md-6 col-md-offset-3 col-xs-12 post-container">
-                <div class="row user">
-                    <div class="col-md-2 col-xs-3 user-pic">
-                        <img class="img-circle user-pic" src="img/1.jpg">
+        <?php include 'include/navbar.php'?>
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4 profile-pic">
+                        <img class="img-circle profile-pic" src="img/1.jpg">
                     </div>
-                    <div class="col-md-6 col-xs-4 username">
+                    <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4 username">
                         <h4 style="font-family: cursive; font-weight: bold;">Jin</h4>
                     </div>
-                    <div class="col-md-4 col-xs-5 location">
+                    <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4 location">
                         <h5 class="pull-right" style="font-weight: bold;"><i class="glyphicon glyphicon-map-marker"></i>&nbsp;ireland</h5>
                     </div>
                 </div>
-                <div class="img-container">
-                    <img src="img/bg.jpg" alt="" class="img-responsive"/>
-                </div>
-            </div>       
-        </div>
+            </div>
     </body>
 </html>
