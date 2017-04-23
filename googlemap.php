5817aa505be90c861b86cc1fd205144e5b17f879
Jin <jinlim0101@hotmail.com>
2017-04-23 21:57:51 +0100

    google map icon

--- googlemap.php
+++ googlemap.php
@@ -40,15 +40,15 @@ and open the template in the editor.
     <body>
         <?php include 'navbar.php' ?>
         <div class="container img-responsive" id="map"></div>
-        <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+        <input id="pac-input" class="controls" type="hidden" placeholder="Search Box">
         <br>
-        
-            <button class="visible-xs-inline-block visible-sm-inline-block"><i class="fa fa-car" onclick="travelMode = 'DRIVING';calculateRoute()"></i></button>
-            <button class="visible-xs-inline-block visible-sm-inline-block"><i class="fa fa-subway" onclick="travelMode = 'TRANSIT';calculateRoute()"></i></button>
-            <button class="visible-xs-inline-block visible-sm-inline-block"><i class="fa fa-bicycle" onclick="travelMode = 'BICYCLING';calculateRoute()"></i></button>
-            <button class="visible-xs-inline-block visible-sm-inline-block"><i class="" onclick="travelMode = 'WALKING';calculateRoute()"><img id="walking" src="img/walking.png" alt=""/></i></button>
-        
-            <br><br>
+
+        <button class="visible-xs-inline-block visible-sm-inline-block"><i class="fa fa-car" onclick="travelMode = 'DRIVING';calculateRoute()"></i></button>
+        <button class="visible-xs-inline-block visible-sm-inline-block"><i class="fa fa-subway" onclick="travelMode = 'TRANSIT';calculateRoute()"></i></button>
+        <button class="visible-xs-inline-block visible-sm-inline-block"><i class="fa fa-bicycle" onclick="travelMode = 'BICYCLING';calculateRoute()"></i></button>
+        <button class="visible-xs-inline-block visible-sm-inline-block"><i class="" onclick="travelMode = 'WALKING';calculateRoute()"><img id="walking" src="img/walking.png" alt=""/></i></button>
+
+        <br><br>
         <details class="container visible-xs-inline-block visible-sm-inline-block"><summary>Directions</summary>
             <div class="col-md-12 visible-xs-inline-block visible-sm-inline-block" id="directions"></div>
         </details>
@@ -66,18 +66,31 @@ and open the template in the editor.
             function initAutocomplete() {
                 var latt = <?php echo $location['lattitude'] ?>;
                 var long = <?php echo $location['longtitude'] ?>;
-                if (latt ===  0) {
+                if (latt === 0) {
                     latt = -33.8688;
                 }
                 if (long === 0) {
                     long = 151.2195;
                 }
-                
+
                 var map = new google.maps.Map(document.getElementById('map'), {
                     center: {lat: latt, lng: long},
                     zoom: 18,
                     mapTypeId: 'roadmap'
                 });
+
+                // Create a marker for each place.
+                var location = new google.maps.LatLng(latt, long);
+                var markerImage = 'img/map-icon.png';
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: markerImage,
+                    animation: google.maps.Animation.DROP,
+                    position: new google.maps.LatLng(latt, long),
+                    visible: true
+                });
+
+                marker.setMap(map);
 
                 // Create the search box and link it to the UI element.
                 var input = document.getElementById('pac-input');
@@ -112,21 +125,7 @@ and open the template in the editor.
                             console.log("Returned place contains no geometry");
                             return;
                         }
-                        var icon = {
-                            url: place.icon,
-                            size: new google.maps.Size(71, 71),
-                            origin: new google.maps.Point(0, 0),
-                            anchor: new google.maps.Point(17, 34),
-                            scaledSize: new google.maps.Size(25, 25)
-                        };
 
-                        // Create a marker for each place.
-                        markers.push(new google.maps.Marker({
-                            map: map,
-                            icon: icon,
-                            title: place.name,
-                            position: place.geometry.location
-                        }));
 
                         if (place.geometry.viewport) {
                             // Only geocodes have viewport.
@@ -138,6 +137,25 @@ and open the template in the editor.
                     map.fitBounds(bounds);
                 });
             }
+
+            function calculateRoute()
+            {
+                var start = document.getElementById('start').value;
+                var end = document.getElementById('end').value;
+
+                var request = {origin: start,
+                    destination: end,
+                    travelMode: google.maps.TravelMode[travelMode]};
+
+                directionsService.route(request, function (response, status)
+                {
+                    if (status == google.maps.DirectionsStatus.OK)
+                    {
+                        directionsDisplay.setDirections(response);
+                    }
+                });
+            }
+
 
         </script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAPg0ETIZ5yEVLUw7ibbiKcnOJuTkr_wAA&libraries=places&callback=initAutocomplete"
